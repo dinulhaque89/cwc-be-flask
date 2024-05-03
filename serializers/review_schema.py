ca84077b959f411a23db2cb29f4f338894b67cf4
Dinul Haque <66052039+dinulhaque@users.noreply.github.com>
2024-05-03 19:11:18 +0100

    amended to provide details

--- serializers/review_schema.py
+++ serializers/review_schema.py
@@ -8,5 +8,9 @@ class ReviewSchema(SQLAlchemyAutoSchema):
         load_instance = True
         unknown = EXCLUDE 
     
+    review_id = fields.Integer(dump_only=True)
+    booking_id = fields.Integer()
+    passenger_id = fields.Integer()
+    driver_id = fields.Integer()
     rating = fields.Integer()
     comments = fields.String()
